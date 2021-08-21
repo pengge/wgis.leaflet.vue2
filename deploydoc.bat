5b6f49932477ac867e3c091e7969f662f789845d
pengge <caipeng8@gmail.com>
2021-08-21 17:30:32 +0800

    Create deploydoc.bat
    deploy doc

---
+++ deploydoc.bat
@@ -0,0 +1,21 @@
+##确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹，这里是默认的路径，可以自定义
+cd .vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.isunbeam.cn' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
+git push -f git@github.com:pengge/wgis.leaflet.vue2.git master:doc
+
+cd -
